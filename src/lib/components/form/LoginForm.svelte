13d2637a86fcef7a3eb2b4884121cd16c36eec04
eduar <eduar@EDUARDO>
2026-04-16 18:03:27 -0400

    update

--- src/lib/components/form/LoginForm.svelte
+++ src/lib/components/form/LoginForm.svelte
@@ -7,11 +7,15 @@
 <!-- -->
 
 <div>
-   <label for="email">Email</label>
-   <input id="email" type="text" placeholder="Email">
+   <div>
+      <label for="email">Email</label>
+      <input id="email" type="text" placeholder="Email">
+   </div>
 
-   <label for="password"></label>
-   <input id="password" type="password" placeholder="Password">
+   <div>
+      <label for="password">Password</label>
+      <input id="password" type="password" placeholder="Password">
+   </div>
 
    <button>Login</button>
    <a href="/register">Still don't have an account? Register</a>
