4df33f824946e2e5eaf33a4669239c8f01a709eb
eduar <eduar@EDUARDO>
2026-04-16 17:55:58 -0400

    update

---
+++ src/lib/components/form/LoginForm.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+</script>
+
+<!-- MARKUP -->
+<!-- -->
+<!-- -->
+
+<div>
+   <input type="text" placeholder="Email">
+   <input type="password" placeholder="Password">
+   <button>Login</button>
+   <a href="/register">Still don't have an account? Register</a>
+</div>
+
+<style>
+
+</style>
---
+++ src/routes/session/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import LoginForm from "$lib/components/form/LoginForm.svelte";
+
+</script>
+
+<!-- MARKUP -->
+<!-- -->
+<!-- -->
+
+<div>
+   <LoginForm />
+</div>
+
+<style>
+
+</style>
